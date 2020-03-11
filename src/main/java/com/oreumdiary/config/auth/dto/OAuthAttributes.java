34e55cb05c43e0fe070895302aecf58665305f12
firepizza <dev.firepizza@gmail.com>
2020-03-12 01:35:29 +0900

    네이버로그인

--- src/main/java/com/oreumdiary/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/oreumdiary/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,9 @@ public class OAuthAttributes {
 
     // OAuth2User가 맵형태로 받아와지기 때문에 이 시스템에서 사용할 객체(OAuthAttributes)로 변환해줌.
     public static OAuthAttributes of (String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,6 +41,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    // 네이버에서 받아온 OAuth2User객체를 변환
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
